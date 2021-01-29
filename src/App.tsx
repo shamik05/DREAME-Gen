d18b1e8e704f080ced1e9653aa55ed625062d650
shamik05 <shamik05@hotmail.com>
2021-01-28 19:22:57 -0500

    Removed extra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <>
-    test
-    </>
-  );
-}
+const App = () => (
+  <>
+    <h1>Test</h1>
+  </>
+);
 
 export default App;
